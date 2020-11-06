93b9b3e70864349d175c7075337bc86074f8b009
Тарас Семенченко <semtaras20@gmail.com>
2020-11-06 17:47:20 +0200

    bug fixed

--- Additional_task.sql
+++ Additional_task.sql
@@ -1,23 +1,37 @@
-SELECT DISTINCT v.ID, v.Name
+-- Додає значення рядків, які є в таблиці, але немає в pdf файлі
+
+INSERT INTO semenchenko_schema.Vendor
+SELECT f.VendorID AS ID, v.Name
 FROM (
     SELECT DISTINCT VendorID
     FROM semenchenko_schema.fact_tripdata
 ) AS f
 LEFT JOIN semenchenko_schema.Vendor AS v
 ON f.VendorID = v.ID
+WHERE v.Name IS NULL
 
-SELECT DISTINCT r.ID, r.Name
+SELECT * FROM semenchenko_schema.Vendor
+
+INSERT INTO semenchenko_schema.RateCode
+SELECT f.RateCodeID AS ID, r.Name
 FROM (
     SELECT DISTINCT RateCodeID
     FROM semenchenko_schema.fact_tripdata
 ) AS f
 LEFT JOIN semenchenko_schema.RateCode AS r
 ON f.RateCodeID = r.ID
+WHERE r.Name IS NULL
 
-SELECT DISTINCT p.ID, p.Name
+SELECT * FROM semenchenko_schema.RateCode
+
+INSERT INTO semenchenko_schema.Payment_type
+SELECT f.Payment_type AS ID, p.Name
 FROM (
     SELECT DISTINCT Payment_type
     FROM semenchenko_schema.fact_tripdata
 ) AS f
 LEFT JOIN semenchenko_schema.Payment_type AS p
 ON f.Payment_type = p.ID
+WHERE p.Name IS NULL
+
+SELECT * FROM semenchenko_schema.Payment_type
